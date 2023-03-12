16defaadf523cd335ccf3dab442866cdedaa9c8a
Andrea Solfanelli <andrea.solfanelli@webranking.it>
2023-03-12 15:07:16 +0100

    delete text in form quote

--- src/components/organisms/FormQuote/FormQuote.tsx
+++ src/components/organisms/FormQuote/FormQuote.tsx
@@ -34,12 +34,8 @@ const FormQuote = () => {
         <Form.Group className="mb-3">
             <Form.Label>Quote</Form.Label>
             <Form.Control as="textarea" placeholder="Whatever you are, be a good one." {...register("text")} required />
-            <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-            </Form.Text>
             {errors.text && <p>{String(errors.text?.message)}</p>}
         </Form.Group>
-
         <Form.Group className="mb-3">
             <Form.Label>Author</Form.Label>
             <Form.Control type="text" placeholder="Abraham Lincoln" {...register("author")} />
